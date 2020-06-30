ff57221eaa54812b0a1e739b802f612c8409dac3
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-06-30 21:43:37 +0300

    WeatherModel.cs model implemented.

---
+++ WeatherSample.Models/WeatherModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace WeatherSample.Models
+{
+    public class WeatherModel
+    {
+        public DateTime Date { get; set; }
+        public string Type { get; set; } = null!;
+        public string City { get; set; } = null!;
+        public int UvIndex { get; set; } = 0;
+        public double Precipitation { get; set; } = 0.0;
+        public int Temperature { get; set; } = -1;
+        public int ApparentTemperature { get; set; } = -1;
+    }
+}
