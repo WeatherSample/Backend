693deb5aa34cad11d0983855ace7da937e9a3f23
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-07-01 14:05:01 +0300

    WeatherForecastController.cs controller added.

--- WeatherSample/Controllers/WeatherForecastController.cs
+++ WeatherSample/Controllers/WeatherForecastController.cs
@@ -1,39 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
+using WeatherSample.Models;
 
 namespace WeatherSample.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    [Route("/api/forecast")]
+    public class WeatherController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
+        [HttpGet("/of/{city}")]
+        public async Task<Weathers> Of(string city)
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
-        }
-
-        [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
-        {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+            // todo: return Weather object as json for selected city.
         }
     }
 }
