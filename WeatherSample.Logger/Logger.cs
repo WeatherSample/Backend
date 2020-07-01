fad2c45f18cfd7ffacc8df42b952f9d026f59202
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-07-01 12:07:21 +0300

    Logger module added and implemented.

---
+++ WeatherSample.Logger/Logger.cs
@@ -0,0 +1,17 @@
+﻿using Serilog;
+
+namespace WeatherSample.Logger
+{
+    public static class Logger
+    {
+        private const string Pattern =
+            "{Timestamp:HH:mm:ss.fff} [{Level:u3}][{SourceContext:l}][{NDC:l}]({ThreadId}) {Message}{NewLine}{Exception:l}";
+
+        public static void ConfigureLogger()
+        {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.ColoredConsole(outputTemplate: Pattern)
+                .CreateLogger();
+        }
+    }
+}
