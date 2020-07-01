260258229b8ece59dcf1f0d82ac6194779bbc5f4
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-07-01 14:03:56 +0300

    WeatherCitiesModel.cs implemented.

---
+++ WeatherSample.Models/WeatherCitiesModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace WeatherSample.Models
+{
+    public class WeatherCities
+    {
+        [JsonProperty("cities")] public List<City> Cities { get; set; } = new List<City>();
+    }
+
+    public class City
+    {
+        [JsonProperty("name")] public string Name { get; set; } = string.Empty;
+    }
+}
