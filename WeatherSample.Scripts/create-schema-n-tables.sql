763a100520959763cba3f809aa314ffcf4373992
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-07-04 12:23:46 +0300

    create-schema-n-tables.sql updated.

--- WeatherSample.Scripts/create-schema-n-tables.sql
+++ WeatherSample.Scripts/create-schema-n-tables.sql
@@ -10,9 +10,12 @@ create table forecast
 (
     CityName    varchar(255) not null,
     Description varchar(255) not null,
-    Uv          double       not null,
-    Precip      double       not null,
-    AppTemp     double       not null,
+    WindSpeed   double       not null,
+    Humidity    long         not null,
+    Pressure    long         not null,
+    TempMin     double       not null,
+    TempMax     double       not null,
+    FeelsLike   double       not null,
     Temp        double       not null,
     `LocalTime` varchar(255) not null
 ) engine = INNODB;
